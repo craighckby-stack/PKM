228856a397ac75bdbc82dc6d258732cef0225d1d
Craig Huckerby <craighckby@gmail.com>
2026-09-07 20:01:55 +1000

    [Sovereign Core - gemini-3.6-flash] Initiate Architecture File: storage/write_protect.h

---
+++ storage/write_protect.h
@@ -0,0 +1,3 @@
+// PKM Permanent Write Protection Interface
+// Hardware storage controller (JEDEC UFS/eMMC) permanent write-lock definitions.
+// Used during panic execution to irreversibly lock corrupted boot partitions.
