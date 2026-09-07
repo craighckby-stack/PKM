832468a96cb9bb2822014d2aae0b124f37a7e818
Craig Huckerby <craighckby@gmail.com>
2026-09-07 20:35:07 +1000

    Delete tests/recovery-attempts directory

--- tests/recovery-attempts/test_transplant.sh
+++
@@ -1,2 +0,0 @@
-# Tests hardware chip transplant and chip-off recovery attempts following PKM activation.
-# Verifies that permanent write protection and key zeroization prevent data extraction on donor boards.
